b41f9f883a4c0cddd8cbd733c686ee5fe5d68f71
Alyssa Clarke <alyssa.clarke00@gmail.com>
2019-02-18 15:57:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,5 +20,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string === string.toLowerCase()) return "I can't hear you!";
-  else if (string === "I love you Grandma.") return ""
+  else if (string === "I love you Grandma.") return "I love you, too.";
+  
+  
 }
